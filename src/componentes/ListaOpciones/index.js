17b26a13f306b7323cef73a87c0ca56f45c0ae33
Raul Gomez <rv.gomalm@gmail.com>
2023-10-01 22:16:48 -0600

    Se suben archivos de componentes

---
+++ src/componentes/ListaOpciones/index.js
@@ -0,0 +1,23 @@
+import "./ListaOpciones.css"
+
+const ListaOpciones = (props) => {
+
+    //metodo map -> arreglo.map( (equipo, index) => {
+    //    return <option></option>
+    // } )
+
+    const manejarCambio = (e) => {
+        console.log("cambio",e.target.value)
+        props.actualizarEquipo(e.target.value)
+    }
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select value={props.valor} onChange={manejarCambio}>
+            <option value="" disabled defaultValue="" hidden> Selecionar equipo </option>
+            {props.equipos.map((equipo,index) => <option key={index} value={equipo}>{equipo}</option>)}
+        </select>
+    </div>
+}
+
+export default ListaOpciones
